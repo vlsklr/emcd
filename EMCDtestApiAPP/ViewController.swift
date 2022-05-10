24c00eff0e291b64e40b53393e5d7aa38de333fa
Vladislav Sklyarov <v.sklyarov@trueengineering.ru>
2022-05-10 18:08:45 +0700

    Initial Commit

---
+++ EMCDtestApiAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMCDtestApiAPP
+//
+//  Created by v.sklyarov on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
